a3ecc9196cd2d1141aeb2be492678f30ade0b8c0
SATHISH KUMAR R <68805018+Sathishkumar-R@users.noreply.github.com>
2020-12-17 19:32:50 +0530

    Create Factorial.java

---
+++ PROGRAMS/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Factorial
+{
+	public static void main(String args[])
+	{
+		int n,i=1,f=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter any number to find factorial :");
+		n=sc.nextInt();
+		sc.close();
+		for(i=1;i<=n;i++)
+			f=f*i;
+		System.out.println("Factorial of the number is:"+f);
+	}
+}
